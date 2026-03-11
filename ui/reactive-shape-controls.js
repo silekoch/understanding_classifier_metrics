a97c5ab3d117512c7789d5ae4891468c697064cd
Simon Koch <simon.leo.koch@tum.de>
2026-03-11 20:05:23 +0100

    Refactor shape control wiring to key-driven loops

--- ui/reactive-shape-controls.js
+++ ui/reactive-shape-controls.js
@@ -1,4 +1,4 @@
-import { sanitizeControlValue } from "./control-specs.js";
+import { SHAPE_CONTROL_KEYS, sanitizeControlValue } from "./control-specs.js";
 
 function subscribeShapeControl({ store, key, state, ids, regenerateAndRender }) {
   store.subscribe(key, (nextValue) => {
@@ -9,56 +9,34 @@ function subscribeShapeControl({ store, key, state, ids, regenerateAndRender }) 
 }
 
 export function wireShapeControls({ store, state, ids, regenerateAndRender }) {
-  subscribeShapeControl({ store, key: "p0Neg", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "p0Pos", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "zeroValue", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "alphaNeg", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "betaNeg", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "alphaPos", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "betaPos", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "epsPos", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "epsNeg", state, ids, regenerateAndRender });
-  subscribeShapeControl({ store, key: "confSharpness", state, ids, regenerateAndRender });
+  for (const key of SHAPE_CONTROL_KEYS) {
+    subscribeShapeControl({ store, key, state, ids, regenerateAndRender });
+  }
 
-  const applyShapeControl = (key) => (rawValue) => {
-    store.set(key, sanitizeControlValue(key, rawValue));
-  };
-
-  const applyP0Neg = applyShapeControl("p0Neg");
-  const applyP0Pos = applyShapeControl("p0Pos");
-  const applyZeroValue = applyShapeControl("zeroValue");
-  const applyAlphaNeg = applyShapeControl("alphaNeg");
-  const applyBetaNeg = applyShapeControl("betaNeg");
-  const applyAlphaPos = applyShapeControl("alphaPos");
-  const applyBetaPos = applyShapeControl("betaPos");
-  const applyEpsPos = applyShapeControl("epsPos");
-  const applyEpsNeg = applyShapeControl("epsNeg");
-  const applyConfSharpness = applyShapeControl("confSharpness");
+  const applyShapeByKey = {};
+  for (const key of SHAPE_CONTROL_KEYS) {
+    applyShapeByKey[key] = (rawValue) => {
+      store.set(key, sanitizeControlValue(key, rawValue));
+    };
+  }
 
   function syncShapeControlsToStore() {
-    store.set("p0Neg", state.p0Neg, { silent: true });
-    store.set("p0Pos", state.p0Pos, { silent: true });
-    store.set("zeroValue", state.zeroValue, { silent: true });
-    store.set("alphaNeg", state.alphaNeg, { silent: true });
-    store.set("betaNeg", state.betaNeg, { silent: true });
-    store.set("alphaPos", state.alphaPos, { silent: true });
-    store.set("betaPos", state.betaPos, { silent: true });
-    store.set("epsPos", state.epsPos, { silent: true });
-    store.set("epsNeg", state.epsNeg, { silent: true });
-    store.set("confSharpness", state.confSharpness, { silent: true });
+    for (const key of SHAPE_CONTROL_KEYS) {
+      store.set(key, state[key], { silent: true });
+    }
   }
 
   return {
-    applyP0Neg,
-    applyP0Pos,
-    applyZeroValue,
-    applyAlphaNeg,
-    applyBetaNeg,
-    applyAlphaPos,
-    applyBetaPos,
-    applyEpsPos,
-    applyEpsNeg,
-    applyConfSharpness,
+    applyP0Neg: applyShapeByKey.p0Neg,
+    applyP0Pos: applyShapeByKey.p0Pos,
+    applyZeroValue: applyShapeByKey.zeroValue,
+    applyAlphaNeg: applyShapeByKey.alphaNeg,
+    applyBetaNeg: applyShapeByKey.betaNeg,
+    applyAlphaPos: applyShapeByKey.alphaPos,
+    applyBetaPos: applyShapeByKey.betaPos,
+    applyEpsPos: applyShapeByKey.epsPos,
+    applyEpsNeg: applyShapeByKey.epsNeg,
+    applyConfSharpness: applyShapeByKey.confSharpness,
     syncShapeControlsToStore,
   };
 }
